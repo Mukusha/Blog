18fb8ce52d0a9b5b8dfc630f4c38e3fe14a06f23
S-W-R <sn_swr1@mail.ru>
2022-11-19 20:29:21 +0500

    try fix csrf

--- src/main/java/com/smile/blog/configs/WebSecurityConfig.java
+++ src/main/java/com/smile/blog/configs/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -22,27 +23,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     //сконфигурировать доступность путей разным ролям, например так
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    //Доступ разрешен всем пользователей
-                    .antMatchers("/login", "/registration").permitAll()
-                    //Все остальные страницы требуют аутентификации
-                    .anyRequest().authenticated()
-                .and()
-                    .csrf().disable() //что бы не падали Post запросы. Это связано с токенами
-                    .formLogin()
-                    .permitAll()
-                .and()
-                    .logout()
-                    .permitAll();
-     //   return http.build();
+        http.authorizeRequests()
+                //Доступ разрешен всем пользователей
+                .antMatchers("/login", "/registration").permitAll()
+                //Все остальные страницы требуют аутентификации
+                .anyRequest().authenticated()
+                //что бы не падали Post запросы. Это связано с токенами
+                .and() .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and().formLogin().permitAll()
+                .and().logout().permitAll();
+        //   return http.build();
     }
 
 
     // сохранение информации о пользователе, что бы получать её в контроллере
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth.userDetailsService(userService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
